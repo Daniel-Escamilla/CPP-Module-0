2d457b7b0cccc3b6a9ef467042d51f81fb1d2e2e
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-21 19:19:23 +0200

    Add include.hpp header file for PhoneBook project

---
+++ ex01/include/include.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 19:01:20 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/21 19:01:21 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include "PhoneBook.hpp"
+
+#endif
