d7cd57dc1866de3a7aad537a1bbdcf0b479fdb93
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-23 21:58:21 +0200

    Update author information and improve code formatting in PhoneBook.cpp

--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -3,11 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 19:01:43 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/21 19:19:57 by descamil         ###   ########.fr       */
+/*   Updated: 2025/04/23 21:48:22 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PhoneBook.hpp"
+
+const std::string PhoneBook::FunctionNames[3] = {"ADD", "SEARCH", "EXIT"};
+
+PhoneBook::Pointer PhoneBook::Functions[3] = {
+	&PhoneBook::AddContact,
+	&PhoneBook::SearchByName,
+	&PhoneBook::Exit
+};
+
+void	PhoneBook::createPhoneBook()
+{
+	std::string input = "";
+	PhoneBook	book;
+		
+	std::cout << "PHONE BOOK\n";
+	while (_running)
+	{
+		std::getline(std::cin, input);
+		if (input.empty())
+			continue ;
+		for (int i = 0; i < 3; i++)
+		{
+			if (input == FunctionNames[i])
+				(this->*Functions[i])();
+		}
+	}
+}
